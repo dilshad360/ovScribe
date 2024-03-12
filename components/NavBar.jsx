0097b1b4ff686bafa07c3f15350263181ead59a1
dilshad360 <dilshad360.dev@gmail.com>
2024-03-13 00:17:42 +0530

    feat: framer-motion

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,13 +4,20 @@ import Link from "next/link";
 import { Button } from "./ui";
 import { useAuth } from "@/app/firebase/auth";
 import Loader from "./loader";
-import { Bookmark, LogOut, Settings, SquarePen, UserRound, X } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  Bookmark,
+  LogOut,
+  Settings,
+  SquarePen,
+  UserRound,
+  X,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useState } from "react";
 import { Badge } from "./ui/badge";
+import { motion } from "framer-motion";
 
 const NavBar = () => {
-
   const { authUser, isloading, signOut } = useAuth();
   const [showProfilePanel, setShowProfilePanel] = useState(false);
 
@@ -27,83 +34,109 @@ const NavBar = () => {
         <div className="flex flex-row gap-3 m-4">
           {authUser ? (
             <>
-
-              <Link href="/user/bookmark" className="flex justify-center items-center">
+              <Link
+                href="/user/bookmark"
+                className="flex justify-center items-center"
+              >
                 <Bookmark className="mr-1 w-5 hover:scale-125 hover:fill-black transition-all ease-in-out " />
               </Link>
 
               <Link href="/user/create-post">
-                <Button variant="outline"> <SquarePen className="mr-1 w-4" /> Write</Button>
+                <Button variant="outline">
+                  {" "}
+                  <SquarePen className="mr-1 w-4" /> Write
+                </Button>
               </Link>
 
               <div>
-
-                <Avatar className="cursor-pointer hover:shadow-md hover:scale-105 transition-all ease-in-out" onClick={() => { setShowProfilePanel(!showProfilePanel) }} >
+                <Avatar
+                  className="cursor-pointer hover:shadow-md hover:scale-105 transition-all ease-in-out"
+                  onClick={() => {
+                    setShowProfilePanel(!showProfilePanel);
+                  }}
+                >
                   <AvatarImage src={authUser.profileImage} />
                   {authUser.username && (
                     <>
-                      {!showProfilePanel ?
-                        <AvatarFallback className="bg-white border font-semibold" >{authUser.username.substring(0, 2).toUpperCase()}</AvatarFallback> :
-                        <AvatarFallback className="bg-black border font-semibold text-white" >< X /></AvatarFallback>
-                      }
+                      {!showProfilePanel ? (
+                        <AvatarFallback className="bg-white border font-semibold">
+                          {authUser.username.substring(0, 2).toUpperCase()}
+                        </AvatarFallback>
+                      ) : (
+                        <AvatarFallback className="bg-black border font-semibold text-white">
+                          <X />
+                        </AvatarFallback>
+                      )}
                     </>
                   )}
                 </Avatar>
 
-                {showProfilePanel &&
-                  <div className="w-[280px] bg-white absolute right-0 my-2 mx-10 rounded-xl shadow-lg border-2 flex justify-between items-center px-4 py-2 flex-col z-10">
-                    <div className="flex  justify-end w-full">
-                    </div>
+                {showProfilePanel && (
+                  <motion.div
+                    initial={{ opacity: 0, y: -75 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    className="w-[280px] bg-white absolute right-0 my-2 mx-10 rounded-xl shadow-lg border-2 flex justify-between items-center px-4 py-2 flex-col z-10"
+                  >
+                    <div className="flex  justify-end w-full"></div>
                     <div className="flex flex-col items-center pt-6">
-                      <Avatar className="scale-150" >
+                      <Avatar className="scale-150">
                         <AvatarImage src={authUser.profileImage} />
                         {authUser.username && (
-                          <AvatarFallback className="bg-white border font-semibold" >{authUser.username.substring(0, 2).toUpperCase()}</AvatarFallback>
+                          <AvatarFallback className="bg-white border font-semibold">
+                            {authUser.username.substring(0, 2).toUpperCase()}
+                          </AvatarFallback>
                         )}
                       </Avatar>
                       <div className="flex flex-col items-center pt-4 ">
-                        <span className="text-lg font-semibold">{authUser.username}</span>
-                        <span className="text-xs text-gray-500 pb-1" >{authUser.email}</span>
-                        {authUser.role && authUser.role !== "user" &&
-                          <Badge variant="outline">{authUser.role.toUpperCase()}</Badge>
-                        }
+                        <span className="text-lg font-semibold">
+                          {authUser.username}
+                        </span>
+                        <span className="text-xs text-gray-500 pb-1">
+                          {authUser.email}
+                        </span>
+                        {authUser.role && authUser.role !== "user" && (
+                          <Badge variant="outline">
+                            {authUser.role.toUpperCase()}
+                          </Badge>
+                        )}
                       </div>
                     </div>
                     <div className="w-full py-4 space-y-2 ">
-                    <Link href="/user/my-posts">
-                          <Button className="w-full" variant="ghost" >
-                            My posts
-                          </Button>
-                        </Link>
-                      {authUser.role && authUser.role !== "user" &&
+                      <Link href="/user/my-posts">
+                        <Button className="w-full" variant="ghost">
+                          My posts
+                        </Button>
+                      </Link>
+                      {authUser.role && authUser.role !== "user" && (
                         <Link href="/manage/posts">
-                          <Button className="w-full" variant="ghost" >
+                          <Button className="w-full" variant="ghost">
                             Manage posts
                           </Button>
                         </Link>
-                      }
-                      {authUser.role && authUser.role === "admin" &&
+                      )}
+                      {authUser.role && authUser.role === "admin" && (
                         <Link href="/manage/users">
-                          <Button className="w-full" variant="ghost" >
+                          <Button className="w-full" variant="ghost">
                             Manage users
                           </Button>
                         </Link>
-                      }
+                      )}
                       <Link href="/user/settings">
-                      <Button className="w-full" variant="ghost" >
-                        <Settings className="mr-1 scale-75 opacity-75" />
-                        Settings
-                      </Button>
+                        <Button className="w-full" variant="ghost">
+                          <Settings className="mr-1 scale-75 opacity-75" />
+                          Settings
+                        </Button>
                       </Link>
-                      <Button className="w-full text-red-600 border-red-600 hover:text-red-700 " variant="outline" onClick={signOut}>
+                      <Button
+                        className="w-full text-red-600 border-red-600 hover:text-red-700 "
+                        variant="outline"
+                        onClick={signOut}
+                      >
                         <LogOut />
                       </Button>
                     </div>
-
-                  </div>
-                }
-
-
+                  </motion.div>
+                )}
               </div>
             </>
           ) : (
--- components/post/UserProfile.jsx
+++ components/post/UserProfile.jsx
@@ -4,6 +4,7 @@ import { db } from "@/app/firebase/config";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Skeleton } from "../ui/skeleton";
 
 const UserProfile = ({ userId }) => {
     const [userData, setUserData] = useState(null);
@@ -30,7 +31,7 @@ const UserProfile = ({ userId }) => {
 
     return (
         <>
-            {userData && (
+            {userData ? (
                 <div className="flex gap-1 items-center pb-2">
                     <Avatar>
                         <AvatarImage src={userData.profileImage} />
@@ -41,7 +42,10 @@ const UserProfile = ({ userId }) => {
                     </Avatar>
                     <h4 className="font-semibold">{userData.username}</h4>
                 </div>
-            )}
+            ): (<div  className="flex gap-1 items-center pb-2">
+            <Skeleton className="w-[40px] h-[40px] rounded-full" />
+            <Skeleton className="h-3 w-[100px]" />
+            </div>)}
         </>
     );
 };
